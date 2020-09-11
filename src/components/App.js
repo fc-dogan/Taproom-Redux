9d0f94cf92c880011dd98cf65e01afbe3471056d
Fatma <fceyda@gmail.com>
2020-09-11 09:38:49 -0700

    create kegControl class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import KegControl from './KegContol';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      
+      <KegControl />
     </React.Fragment>
   );
 }
---
+++ src/components/KegContol.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class KegControl extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+
+  render(){
+    return (
+      <h1>Keg control</h1>
+      
+    )
+  }
+}
+
+export default KegControl; 
