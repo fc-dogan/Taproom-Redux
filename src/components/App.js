c80f0a41c0882ea5b75bfa0086a8b20a3039eaf4
Fatma <fceyda@gmail.com>
2020-09-11 09:35:34 -0700

    create header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-    <p>App</p>
-    </div>
+    <React.Fragment>
+      <Header />
+      
+    </React.Fragment>
   );
 }
 
