1d59642f1102f4edbb613a39508b3377a211534b
Fatma <fceyda@gmail.com>
2020-09-18 11:49:21 -0700

    add editingKegReducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import formVisibleReducer from './form-visible-reducer';
 import kegListReducer from './/keg-list-reducer';
 import selectKegReducer from './select-keg-reducer';
+import editingKegReducer from'./editing-keg-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
   allKombuchaList: kegListReducer,
-  selectedKeg: selectKegReducer
+  selectedKeg: selectKegReducer,
+  editingKeg: editingKegReducer
 });
 
 export default rootReducer;
