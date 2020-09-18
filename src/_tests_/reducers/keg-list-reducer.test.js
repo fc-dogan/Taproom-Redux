3329802bb8952e6baeb3ea09ecea033663afe2de
Fatma <fceyda@gmail.com>
2020-09-18 08:39:34 -0700

    successfully fail the add a new data test

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -4,4 +4,37 @@ describe('kegListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  let action;
+  const kegData = {
+      name: "Synergy",
+      brand: "GT's",
+      price: 3,
+      flavor: "Cosmic Cranberry",
+      quantity: 120,
+      id: 1
+  };
+
+  test('Should successfully add a new keg data to allKegList', () => {
+    const { name, brand, price, flavor, quantity, id } = kegData;
+    action ={
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      quantity: quantity,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
 });
