4707022c2adfcc0f15d2d66821e7662d54b4d76a
Fatma <fceyda@gmail.com>
2020-09-18 09:40:41 -0700

    pass the test

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,6 +1,10 @@
 import * as a from '../actions/ActionTypes';
 
 export default (state = false, action) => {
-
-  return state;
+  switch (action.type){
+    case a.TOGGLE_FORM :
+      return !state;
+    default:
+      return state;
+  }
 }
