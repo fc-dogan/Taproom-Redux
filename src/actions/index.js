b4cf3bbbe1fdd18590a9f62053946f7ed36e8d04
Fatma <fceyda@gmail.com>
2020-09-18 12:21:25 -0700

    pass the test for addKeg action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,11 @@ export const addKeg = (kegData) =>{
   const { name, brand, price, flavor, quantity, id } = kegData;
   return {
     type: a.ADD_KEG,
-    
+    name: name,
+    brand: brand,
+    price: price,
+    flavor: flavor,
+    quantity: quantity,
+    id: id
   }
 }
