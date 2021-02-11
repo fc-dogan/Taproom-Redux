3f86ec1b23d7c773139beb6cd8ffa8f76220fa89
Fatma <fceyda@gmail.com>
2021-02-11 11:04:34 -0800

    Updates

---
+++ precache-manifest.40c9101fe15829e76753fcf8aa9c193e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3cc74e300773c3ba242fd1deebc09e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b320437bc7ad7bece28",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "12e5577de6492a1ea5dc",
+    "url": "/static/css/main.82ca5109.chunk.css"
+  },
+  {
+    "revision": "5b320437bc7ad7bece28",
+    "url": "/static/js/2.88aa0277.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.88aa0277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e5577de6492a1ea5dc",
+    "url": "/static/js/main.5cd9ff7f.chunk.js"
+  },
+  {
+    "revision": "0386e31d93b0b766d9a7",
+    "url": "/static/js/runtime-main.2165cce1.js"
+  }
+]);
