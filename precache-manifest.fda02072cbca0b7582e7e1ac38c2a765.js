79e22467c0192037ad7a39ef07e2c542b0b36dae
Fatma <fceyda@gmail.com>
2021-02-11 11:09:47 -0800

    Updates

---
+++ precache-manifest.fda02072cbca0b7582e7e1ac38c2a765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a9eb8f57737300925e7848895f6795",
+    "url": "/Taproom-Redux/index.html"
+  },
+  {
+    "revision": "5b320437bc7ad7bece28",
+    "url": "/Taproom-Redux/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d68621807b2bce60ec17",
+    "url": "/Taproom-Redux/static/css/main.82ca5109.chunk.css"
+  },
+  {
+    "revision": "5b320437bc7ad7bece28",
+    "url": "/Taproom-Redux/static/js/2.88aa0277.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Taproom-Redux/static/js/2.88aa0277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68621807b2bce60ec17",
+    "url": "/Taproom-Redux/static/js/main.f4972400.chunk.js"
+  },
+  {
+    "revision": "a27589d26a2b8c4992fc",
+    "url": "/Taproom-Redux/static/js/runtime-main.a17d6eb3.js"
+  }
+]);
